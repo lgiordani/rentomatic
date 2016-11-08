e0a3f846036a8e14abc46086cc077b3b590d9404
Leonardo Giordani <giordani.leonardo@gmail.com>
2016-11-08 09:45:07 +0000

    Responses and failures

--- rentomatic/shared/response_object.py
+++ rentomatic/shared/response_object.py
@@ -1,9 +1,51 @@
 class ResponseSuccess(object):
+    SUCCESS = 'SUCCESS'
 
     def __init__(self, value=None):
+        self.type = self.SUCCESS
         self.value = value
 
     def __nonzero__(self):
         return True
 
     __bool__ = __nonzero__
+
+
+class ResponseFailure(object):
+    RESOURCE_ERROR = 'RESOURCE_ERROR'
+    PARAMETERS_ERROR = 'PARAMETERS_ERROR'
+    SYSTEM_ERROR = 'SYSTEM_ERROR'
+
+    def __init__(self, type_, message):
+        self.type = type_
+        self.message = self._format_message(message)
+
+    def _format_message(self, msg):
+        if isinstance(msg, Exception):
+            return "{}: {}".format(msg.__class__.__name__, "{}".format(msg))
+        return msg
+
+    @property
+    def value(self):
+        return {'type': self.type, 'message': self.message}
+
+    def __bool__(self):
+        return False
+
+    @classmethod
+    def build_resource_error(cls, message=None):
+        return cls(cls.RESOURCE_ERROR, message)
+
+    @classmethod
+    def build_system_error(cls, message=None):
+        return cls(cls.SYSTEM_ERROR, message)
+
+    @classmethod
+    def build_parameters_error(cls, message=None):
+        return cls(cls.PARAMETERS_ERROR, message)
+
+    @classmethod
+    def build_from_invalid_request_object(cls, invalid_request_object):
+        message = "\n".join(["{}: {}".format(err['parameter'], err['message'])
+                             for err in invalid_request_object.errors])
+        return cls.build_parameters_error(message)
