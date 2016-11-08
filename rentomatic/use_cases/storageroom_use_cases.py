d902a90a9ab3b9b147aea29c1690ac43dcad6074
Leonardo Giordani <giordani.leonardo@gmail.com>
2016-11-08 10:56:01 +0000

    Use cases (part 3)

--- rentomatic/use_cases/storageroom_use_cases.py
+++ rentomatic/use_cases/storageroom_use_cases.py
@@ -1,4 +1,4 @@
-from rentomatic.shared import response_object as ro
+from rentomatic.shared import response_object as res
 
 
 class StorageRoomListUseCase(object):
@@ -7,5 +7,12 @@ class StorageRoomListUseCase(object):
         self.repo = repo
 
     def execute(self, request_object):
-        storage_rooms = self.repo.list()
-        return ro.ResponseSuccess(storage_rooms)
+        if not request_object:
+            return res.ResponseFailure.build_from_invalid_request_object(request_object)
+
+        try:
+            storage_rooms = self.repo.list(filters=request_object.filters)
+            return res.ResponseSuccess(storage_rooms)
+        except Exception as exc:
+            return res.ResponseFailure.build_system_error(
+                "{}: {}".format(exc.__class__.__name__, "{}".format(exc)))
